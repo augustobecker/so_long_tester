f202ae3cf508beb37a9b549410ff461a2b39a9ad
augustobecker <augustobecker.dev@gmail.com>
2022-03-16 02:20:00 -0300

    Add tests for Invalid Maps

---
+++ tests/check-for-invalid-parameter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+#
+#                                                        :::      ::::::::
+#   SoLongTester/check-for-invalid-parameter.sh         :+:      :+:    :+:
+#                                                    +:+ +:+         +:+
+#   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
+#                                                +#+#+#+#+#+   +#+
+#   Created: 2021/03/15 14:35:10 by acesar-l          #+#    #+#
+#   Updated: 2021/03/15 23:58:41 by acesar-l         ###   ########.fr
+#
+#
+
+GREEN="\033[0;32m"
+  RED="\033[0;31m"
+RESET="\033[0m"
+  MAP="/maps/map-invalid-parameter.ber"
+ERROR=$(./so_long $MAP | grep "Error" | wc -l)
+
+if [ ${ERROR} -ge 1 ]
+then
+	echo -e "Check invalid parameter: $GREEN [OK] $RESET"
+else
+	echo -e "Check invalid parameter: $RED [KO] $RESET"
+fi
