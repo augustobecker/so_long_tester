f1138ba1a73a9eda541f3e496af129834c0e0fd8
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-25 09:36:45 -0300

    Update check-for-invalid-extension.sh

--- tests/check-for-invalid-extension.sh
+++ tests/check-for-invalid-extension.sh
@@ -8,15 +8,15 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
 #                                                +#+#+#+#+#+   +#+
 #   Created: 2021/03/15 14:35:10 by acesar-l          #+#    #+#
-#   Updated: 2021/03/15 23:58:41 by acesar-l         ###   ########.fr
+#   Updated: 2021/03/25 09:36:41 by acesar-l         ###   ########.fr
 #
 #
 
 GREEN="\033[0;32m"
   RED="\033[0;31m"
 RESET="\033[0m"
-  MAP="/maps/map-invalid-extension"
-ERROR=$(./so_long $MAP | grep "Error" | wc -l)
+  MAP="../maps/invalid/map-invalid-extension"
+ERROR=$(./../../so_long $MAP | grep "Error" | wc -l)
 
 if [ ${ERROR} -ge 1 ]
 then
