0a8119c4bb3363101db4ada7f195ed7ca281e44f
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-27 19:28:35 -0300

    Update check-for-missing-W.sh

--- tests/check-for-missing-W.sh
+++ tests/check-for-missing-W.sh
@@ -8,19 +8,19 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
 #                                                +#+#+#+#+#+   +#+
 #   Created: 2021/03/15 14:35:10 by acesar-l          #+#    #+#
-#   Updated: 2021/03/15 23:58:41 by acesar-l         ###   ########.fr
+#   Updated: 2021/03/27 19:28:41 by acesar-l         ###   ########.fr
 #
 #
 
 GREEN="\033[0;32m"
   RED="\033[0;31m"
 RESET="\033[0m"
-  MAP="/maps/map-missing-W.ber"
-ERROR=$(./so_long $MAP | grep "Error" | wc -l)
+  MAP="../maps/invalid/map-missing-W.ber"
+ERROR=$(./../../so_long $MAP  | grep "Error" | wc -l)
 
 if [ ${ERROR} -ge 1 ]
 then
-	echo -e "Check for missing W: $GREEN [OK] $RESET"
+	echo -e "Check for missing W: $GREEN           [OK] $RESET"
 else
-	echo -e "Check for missing W: $RED [KO] $RESET"
+	echo -e "Check for missing W: $RED           [KO] $RESET"
 fi
