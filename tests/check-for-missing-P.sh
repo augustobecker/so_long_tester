6c76c3e69d470c2088a5c9352e3d2fc0003cd15f
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-27 19:29:05 -0300

    Update check-for-missing-P.sh

--- tests/check-for-missing-P.sh
+++ tests/check-for-missing-P.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 #
-#
-#                                                        :::      ::::::::
-#   SoLongTester/check-for-missing-P.sh                 :+:      :+:    :+:
-#                                                    +:+ +:+         +:+
-#   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
-#                                                +#+#+#+#+#+   +#+
-#   Created: 2021/03/15 14:35:10 by acesar-l          #+#    #+#
-#   Updated: 2021/03/15 23:58:41 by acesar-l         ###   ########.fr
-#
-#
+#                                                                            
+#                                                        :::      ::::::::  
+#   SoLongTester/check-for-missing-C.sh                 :+:      :+:    :+: 
+#                                                    +:+ +:+         +:+    
+#   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+      
+#                                                +#+#+#+#+#+   +#+         
+#   Created: 2021/03/15 14:35:10 by acesar-l          #+#    #+#           
+#   Updated: 2021/03/27 19:28:41 by acesar-l         ###   ########.fr     
+#                                                                           
+# 
 
 GREEN="\033[0;32m"
   RED="\033[0;31m"
 RESET="\033[0m"
-  MAP="/maps/map-missing-P.ber"
-ERROR=$(./so_long $MAP | grep "Error" | wc -l)
+  MAP="../maps/invalid/map-missing-C.ber"
+ERROR=$(./../../so_long $MAP | grep "Error" | wc -l)
 
 if [ ${ERROR} -ge 1 ]
 then
-	echo -e "Check for missing P: $GREEN [OK] $RESET"
+	echo -e "Check for missing C: $GREEN           [OK] $RESET"
 else
-	echo -e "Check for missing P: $RED [KO] $RESET"
+	echo -e "Check for missing C: $RED           [KO] $RESET"
 fi
