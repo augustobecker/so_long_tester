0a4890ea7c0eb3df9444d3fd9e3027c10c52327a
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-27 19:32:00 -0300

    Update check-for-missing-C.sh

--- tests/check-for-missing-C.sh
+++ tests/check-for-missing-C.sh
@@ -8,19 +8,19 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+      
 #                                                +#+#+#+#+#+   +#+         
 #   Created: 2021/03/15 14:35:10 by acesar-l          #+#    #+#           
-#   Updated: 2021/03/15 23:58:41 by acesar-l         ###   ########.fr     
+#   Updated: 2021/03/27 19:32:41 by acesar-l         ###   ########.fr     
 #                                                                           
 # 
 
 GREEN="\033[0;32m"
   RED="\033[0;31m"
 RESET="\033[0m"
-  MAP="/maps/map-missing-C.ber"
-ERROR=$(./so_long $MAP | grep "Error" | wc -l)
+  MAP="../maps/invalid/map-missing-C.ber"
+ERROR=$(./../../so_long $MAP | grep "Error" | wc -l)
 
 if [ ${ERROR} -ge 1 ]
 then
-	echo -e "Check for missing C: $GREEN [OK] $RESET"
+	echo -e "Check for missing C: $GREEN           [OK] $RESET"
 else
-	echo -e "Check for missing C: $RED [KO] $RESET"
+	echo -e "Check for missing C: $RED           [KO] $RESET"
 fi
