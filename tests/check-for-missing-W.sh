5ebfe5738ce6a294f86b52d7938e0b5e233f205b
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-27 19:39:46 -0300

    Update check-for-missing-W.sh

--- tests/check-for-missing-W.sh
+++ tests/check-for-missing-W.sh
@@ -8,7 +8,7 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
 #                                                +#+#+#+#+#+   +#+
 #   Created: 2021/03/15 14:35:10 by acesar-l          #+#    #+#
-#   Updated: 2021/03/27 19:28:41 by acesar-l         ###   ########.fr
+#   Updated: 2021/03/27 19:38:41 by acesar-l         ###   ########.fr
 #
 #
 
@@ -16,7 +16,7 @@ GREEN="\033[0;32m"
   RED="\033[0;31m"
 RESET="\033[0m"
   MAP="../maps/invalid/map-missing-W.ber"
-ERROR=$(./../../so_long $MAP  | grep "Error" | wc -l)
+ERROR=$(./../../so_long $MAP | grep "Error" | wc -l)
 
 if [ ${ERROR} -ge 1 ]
 then
