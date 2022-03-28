4e6b69cfcc702163f1d87dcf6378253a911bf3bd
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-28 08:07:47 -0300

    Create check-for-missing-argv.sh

---
+++ tests/check-for-missing-argv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+#
+#                                                        :::      ::::::::
+#   SoLongTester/check-for-missing-argv.sh              :+:      :+:    :+:
+#                                                    +:+ +:+         +:+
+#   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
+#                                                +#+#+#+#+#+   +#+
+#   Created: 2021/03/28 08:07:10 by acesar-l          #+#    #+#
+#   Updated: 2021/03/28 08:08:41 by acesar-l         ###   ########.fr
+#
+#
+
+GREEN="\033[0;32m"
+  RED="\033[0;31m"
+RESET="\033[0m"
+ERROR=$(./../../so_long | grep "Error" | wc -l)
+
+if [ ${ERROR} -ge 1 ]
+then
+	echo -e "Check for missing argv: $GREEN		   [OK] $RESET"
+else
+	echo -e "Check for missing argv: $RED        [KO] $RESET"
+fi
