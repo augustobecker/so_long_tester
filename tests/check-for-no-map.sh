ad46f97b4b36e181251023e7aaf6266120b7d4ae
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-28 07:54:37 -0300

    Create check-for-no-map.sh

---
+++ tests/check-for-no-map.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+#
+#                                                        :::      ::::::::
+#   SoLongTester/check-for-no-map.sh                    :+:      :+:    :+:
+#                                                    +:+ +:+         +:+
+#   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
+#                                                +#+#+#+#+#+   +#+
+#   Created: 2021/03/28 07:52:10 by acesar-l          #+#    #+#
+#   Updated: 2021/03/28 07:52:41 by acesar-l         ###   ########.fr
+#
+#
+
+GREEN="\033[0;32m"
+  RED="\033[0;31m"
+RESET="\033[0m"
+  MAP="../maps/invalid/no-map.ber"
+ERROR=$(./../../so_long $MAP | grep "Error" | wc -l)
+
+if [ ${ERROR} -ge 1 ]
+then
+	echo -e "Check for no Map: $GREEN		       [OK] $RESET"
+else
+	echo -e "Check for no Map: $RED              [KO] $RESET"
+fi
