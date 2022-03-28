a0d4d6094db83cde89f9a7add211bf60cb32bc97
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-27 21:06:35 -0300

    Update check-for-square-map.sh

--- tests/check-for-square-map.sh
+++ tests/check-for-square-map.sh
@@ -8,7 +8,7 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
 #                                                +#+#+#+#+#+   +#+
 #   Created: 2021/03/15 14:35:10 by acesar-l          #+#    #+#
-#   Updated: 2021/03/27 19:28:41 by acesar-l         ###   ########.fr
+#   Updated: 2021/03/27 21:08:41 by acesar-l         ###   ########.fr
 #
 #
 
@@ -20,7 +20,7 @@ ERROR=$(./../../so_long $MAP | grep "Error" | wc -l)
 
 if [ ${ERROR} -ge 1 ]
 then
-	echo -e "Check for square Map: $GREEN		[OK] $RESET"
+	echo -e "Check for square Map: $GREEN		   [OK] $RESET"
 else
-	echo -e "Check for square Map: $RED       [KO] $RESET"
+	echo -e "Check for square Map: $RED          [KO] $RESET"
 fi
