ce4548115844044a7717ccc26b6b89f387492892
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-30 00:55:35 -0300

    Update check-for-no-map.sh

--- tests/check-for-no-map.sh
+++ tests/check-for-no-map.sh
@@ -8,7 +8,7 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
 #                                                +#+#+#+#+#+   +#+
 #   Created: 2021/03/28 07:52:10 by acesar-l          #+#    #+#
-#   Updated: 2021/03/30 00:33:41 by acesar-l         ###   ########.fr
+#   Updated: 2021/03/30 00:55:41 by acesar-l         ###   ########.fr
 #
 #
 
@@ -20,7 +20,7 @@ ERROR=$(./../../so_long $MAP | grep "Error" | wc -l)
 
 if [ ${ERROR} -ge 1 ]
 then
-	echo -e "Check for no Map: $GREEN		       [OK] $RESET"
+	echo -e "Check for no Map: $GREEN		[OK] $RESET"
 else
 	echo -e "Check for no Map: $RED              [KO] $RESET"
 fi
