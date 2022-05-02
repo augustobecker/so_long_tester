f13c183b6d5d16c3862fa5b1b7198138677d5e67
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-05-02 13:11:15 -0300

    Update check-for-missing-P.sh

--- tests/check-for-missing-P.sh
+++ tests/check-for-missing-P.sh
@@ -8,19 +8,19 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
 #                                                +#+#+#+#+#+   +#+
 #   Created: 2021/03/15 14:35:10 by acesar-l          #+#    #+#
-#   Updated: 2021/03/30 01:39:41 by acesar-l         ###   ########.fr
+#   Updated: 2021/05/02 13:11:41 by acesar-l         ###   ########.fr
 #
 #
 
 GREEN="\033[0;32m"
   RED="\033[0;31m"
 RESET="\033[0m"
-  MAP="maps/invalid/map-missing-C.ber"
+  MAP="maps/invalid/map-missing-P.ber"
 ERROR=$(./../so_long $MAP | grep "Error" | wc -l)
 
 if [ ${ERROR} -ge 1 ]
 then
-	echo -e "Check for missing C: $GREEN           [OK] $RESET"
+	echo -e "Check for missing P: $GREEN           [OK] $RESET"
 else
-	echo -e "Check for missing C: $RED           [KO] $RESET"
+	echo -e "Check for missing P: $RED           [KO] $RESET"
 fi
