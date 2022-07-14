2d4143701fb3470d016bfc7a1e1399f28d3d4cb0
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-07-14 15:18:24 -0300

    Update check-for-empty-line.sh

--- tests/check-for-empty-line.sh
+++ tests/check-for-empty-line.sh
@@ -8,19 +8,37 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
 #                                                +#+#+#+#+#+   +#+
 #   Created: 2021/06/20 23:43:10 by acesar-l          #+#    #+#
-#   Updated: 2021/06/20 23:43:41 by acesar-l         ###   ########.fr
+#   Updated: 2021/07/14 23:43:41 by acesar-l         ###   ########.fr
 #
 #
 
 GREEN="\033[0;32m"
   RED="\033[0;31m"
 RESET="\033[0m"
-  MAP="maps/valid/map-empty-line.ber"
-ERROR=$(./../so_long $MAP | grep "Error" | wc -l)
+  MAP1="maps/valid/map-empty-line1.ber"
+  MAP2="maps/valid/map-empty-line2.ber"
+  MAP3="maps/valid/map-empty-line3.ber"
+ERROR1=$(./../so_long $MAP1 | grep "Error" | wc -l)
+ERROR2=$(./../so_long $MAP2 | grep "Error" | wc -l)
+ERROR3=$(./../so_long $MAP3 | grep "Error" | wc -l)
 
-if [ ${ERROR} -ge 1 ]
+if [ ${ERROR1} -ge 1 ]
 then
-	echo -e "Check Map with an Empty Line: $RED[KO] $RESET"
+	echo -e -n "Check Map with an Empty Line: $GREEN  [OK] $RESET"
 else
-	echo -e "Check Map with an Empty Line: $GREEN[OK] $RESET"
+	echo -e -n "Check Map with an Empty Line: $RED  [KO] $RESET"
+fi
+
+if [ ${ERROR2} -ge 1 ]
+then
+	echo -e -n "$GREEN[OK] $RESET"
+else
+	echo -e -n "$RED[KO] $RESET"
+fi
+
+if [ ${ERROR3} -ge 1 ]
+then
+	echo -e "$GREEN[OK] $RESET"
+else
+	echo -e "$RED[KO] $RESET"
 fi
