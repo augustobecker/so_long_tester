a1126f172fc0eb88abf33a65bd8d1ac36518da3f
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-07-15 10:41:58 -0300

    Update check-for-multiple-argv.sh

--- tests/check-for-multiple-argv.sh
+++ tests/check-for-multiple-argv.sh
@@ -8,10 +8,11 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
 #                                                +#+#+#+#+#+   +#+
 #   Created: 2021/03/28 08:11:10 by acesar-l          #+#    #+#
-#   Updated: 2021/03/30 01:39:41 by acesar-l         ###   ########.fr
+#   Updated: 2021/07/17 01:39:41 by acesar-l         ###   ########.fr
 #
 #
 
+ CYAN="\033[0;36m"
 GREEN="\033[0;32m"
   RED="\033[0;31m"
 RESET="\033[0m"
@@ -21,7 +22,7 @@ ERROR=$(./../so_long $MAP $EXTRA | grep "Error" | wc -l)
 
 if [ ${ERROR} -ge 1 ]
 then
-	echo -e "Check for multiple argv: $GREEN       [OK] $RESET"
+	echo -e "${CYAN}Check for multiple argv: $GREEN       [OK] $RESET"
 else
-	echo -e "Check for multiple argv: $RED       [KO] $RESET"
+	echo -e "${CYAN}Check for multiple argv: $RED       [KO] $RESET"
 fi
