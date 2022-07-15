134c0d21cd3b63d2c9afff59f1bdc1454c510895
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-07-15 11:07:08 -0300

    Update check-for-invalid-extension.sh

--- tests/check-for-invalid-extension.sh
+++ tests/check-for-invalid-extension.sh
@@ -16,12 +16,39 @@
 GREEN="\033[0;32m"
   RED="\033[0;31m"
 RESET="\033[0m"
-  MAP="maps/invalid/map-invalid-extension"
-ERROR=$(./../so_long $MAP | grep "Error" | wc -l)
+  MAP1="maps/invalid/map-invalid-extension"
+  MAP2="maps/invalid/map-invalid-extension.be"
+  MAP3="maps/invalid/map-invalid-extension.ber.ber"
+  MAP4="maps/invalid/map-invalid-extension.berber"
+ERROR1=$(./../so_long $MAP1 | grep "Error" | wc -l)
+ERROR2=$(./../so_long $MAP2 | grep "Error" | wc -l)
+ERROR3=$(./../so_long $MAP3 | grep "Error" | wc -l)
+ERROR4=$(./../so_long $MAP4 | grep "Error" | wc -l)
 
-if [ ${ERROR} -ge 1 ]
+if [ ${ERROR1} -ge 1 ]
 then
-	echo -e "${CYAN}Check invalid extension: $GREEN       [OK] $RESET"
+	echo -e -n "${CYAN}Check invalid extension: $GREEN       [OK] $RESET"
 else
-	echo -e "${CYAN}Check invalid extension: $RED       [KO] $RESET"
+	echo -e -n "${CYAN}Check invalid extension: $RED       [KO] $RESET"
+fi
+
+if [ ${ERROR2} -ge 1 ]
+then
+	echo -e -n "$GREEN[OK] $RESET"
+else
+	echo -e -n "$RED[KO] $RESET"
+fi
+
+if [ ${ERROR3} -ge 1 ]
+then
+	echo -e -n "$GREEN[OK] $RESET"
+else
+	echo -e -n "$RED[KO] $RESET"
+fi
+
+if [ ${ERROR4} -ge 1 ]
+then
+	echo -e "$GREEN[OK] $RESET"
+else
+	echo -e "$RED[KO] $RESET"
 fi
