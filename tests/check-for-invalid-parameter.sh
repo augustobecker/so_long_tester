16846be29d49fcca3f4b82eb1cf796df4229b0a2
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-07-15 10:37:41 -0300

    Update check-for-invalid-parameter.sh

--- tests/check-for-invalid-parameter.sh
+++ tests/check-for-invalid-parameter.sh
@@ -8,19 +8,20 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+
 #                                                +#+#+#+#+#+   +#+
 #   Created: 2021/03/15 14:35:10 by acesar-l          #+#    #+#
-#   Updated: 2021/03/30 01:39:41 by acesar-l         ###   ########.fr
+#   Updated: 2021/07/17 01:39:41 by acesar-l         ###   ########.fr
 #
 #
 
-	  GREEN="\033[0;32m"
-  		RED="\033[0;31m"
-	  RESET="\033[0m"
-  		MAP="maps/invalid/map-extra-parameter.ber"
-	  ERROR=$(./../so_long $MAP | grep "Error" | wc -l)
+ CYAN="\033[0;36m"
+GREEN="\033[0;32m"
+  RED="\033[0;31m"
+RESET="\033[0m"
+  MAP="maps/invalid/map-extra-parameter.ber"
+ERROR=$(./../so_long $MAP | grep "Error" | wc -l)
 
 if [ ${ERROR} -ge 1 ]
 then
-	echo -e "Check extra parameter: $GREEN         [OK] $RESET"
+	echo -e "${CYAN}Check extra parameter: $GREEN         [OK] $RESET"
 else
-	echo -e "Check extra parameter: $RED         [KO] $RESET"
+	echo -e "${CYAN}Check extra parameter: $RED         [KO] $RESET"
 fi
