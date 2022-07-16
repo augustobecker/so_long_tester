ef1206d15cf991569d299325a7468b118c4d4ce7
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-07-16 01:13:27 -0300

    Update check-for-invalid-parameter.sh

--- tests/check-for-invalid-parameter.sh
+++ tests/check-for-invalid-parameter.sh
@@ -12,16 +12,52 @@
 #
 #
 
- CYAN="\033[0;36m"
-GREEN="\033[0;32m"
-  RED="\033[0;31m"
-RESET="\033[0m"
-  MAP="maps/invalid/map-extra-parameter.ber"
-ERROR=$(./../so_long $MAP | grep "Error" | wc -l)
+  CYAN="\033[0;36m"
+ GREEN="\033[0;32m"
+   RED="\033[0;31m"
+ RESET="\033[0m"
+  MAP1="maps/invalid/map-extra-parameter-1.ber"
+  MAP2="maps/invalid/map-extra-parameter-2.ber"
+  MAP3="maps/invalid/map-extra-parameter-3.ber"
+  MAP4="maps/invalid/map-extra-parameter-4.ber"
+  MAP5="maps/invalid/map-extra-parameter-5.ber"  
+ERROR1=$(./../so_long $MAP1 | grep "Error" | wc -l)
+ERROR2=$(./../so_long $MAP2 | grep "Error" | wc -l)
+ERROR3=$(./../so_long $MAP3 | grep "Error" | wc -l)
+ERROR4=$(./../so_long $MAP4 | grep "Error" | wc -l)
+ERROR5=$(./../so_long $MAP5 | grep "Error" | wc -l)
 
-if [ ${ERROR} -ge 1 ]
+if [ ${ERROR1} -ge 1 ]
 then
-	echo -e "${CYAN}Check extra parameter: $GREEN         [OK] $RESET"
+	echo -e -n "${CYAN}Check extra parameter: $GREEN         [OK] $RESET"
 else
-	echo -e "${CYAN}Check extra parameter: $RED         [KO] $RESET"
+	echo -e -n "${CYAN}Check extra parameter: $RED         [KO] $RESET"
+fi
+
+if [ ${ERROR2} -ge 1 ]
+then
+	echo -e -n "$GREEN[OK] $RESET"
+else
+	echo -e -n "$RED[KO] $RESET"
+fi
+
+if [ ${ERROR3} -ge 1 ]
+then
+	echo -e -n "$GREEN[OK] $RESET"
+else
+	echo -e -n "$RED[KO] $RESET"
+fi
+
+if [ ${ERROR4} -ge 1 ]
+then
+	echo -e -n "$GREEN[OK] $RESET"
+else
+	echo -e -n "$RED[KO] $RESET"
+fi
+
+if [ ${ERROR5} -ge 1 ]
+then
+	echo -e "$GREEN[OK] $RESET"
+else
+	echo -e "$RED[KO] $RESET"
 fi
