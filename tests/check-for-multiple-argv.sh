9fc599c93a7a6e4f5b0f2a4d85344008dacc035b
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-07-18 02:30:17 -0300

    Update check-for-multiple-argv.sh

--- tests/check-for-multiple-argv.sh
+++ tests/check-for-multiple-argv.sh
@@ -19,18 +19,22 @@ RESET="\033[0m"
   MAP="maps/valid/map1.ber"
 EXTRA1="maps/valid/map2.ber"
 EXTRA2="maps/valid/map3.ber"
-ERROR1=$(./../so_long $MAP $EXTRA1 | grep "Error" | wc -l)
-ERROR2=$(./../so_long $MAP $EXTRA1 $EXTRA2 | grep "Error" | wc -l)
+  VAL1=$(valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --quiet --tool=memcheck --keep-debuginfo=yes ./../so_long $MAP $EXTRA1 | grep "Error" > check)
+  VAL2=$(valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --quiet --tool=memcheck --keep-debuginfo=yes ./../so_long $MAP $EXTRA1 $EXTRA2 | grep "Error" > check)
+ ERROR=$(grep "Error" check | wc -l)
 
-if [ ${ERROR1} -ge 1 ]
+${VAL1}
+
+if [ ${ERROR} -ge 1 ]
 then
 	echo -e -n "${CYAN}Check for multiple argv: $GREEN       [OK] $RESET"
 else
 	echo -e -n "${CYAN}Check for multiple argv: $RED       [KO] $RESET"
 fi
 
+${VAL2}
 
-if [ ${ERROR2} -ge 1 ]
+if [ ${ERROR} -ge 1 ]
 then
 	echo -e "$GREEN[OK] $RESET"
 else
